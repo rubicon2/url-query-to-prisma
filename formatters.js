4626528a74ded59701c02ae13c720e5c04b69cdf
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2025-05-01 09:26:12 +0100

    Rename parameter to differentiate from formatters and their purpose

--- formatters.js
+++ formatters.js
@@ -1,13 +1,13 @@
 // Helper functions for customizing with customFormatter parameter.
 // Options on the text is basically just to allow mode: 'insensitive',
 // which is required on postgreSQL or mongoDB to case-insensitive filtering.
-function where(filterType, options = {}, valueFormatter = (value) => value) {
+function where(filterType, options = {}, valueProcessor = (value) => value) {
   if (filterType) {
     return (queryObj, key, value) => {
       queryObj.where = {
         ...queryObj.where,
         [key]: {
-          [filterType]: valueFormatter(value),
+          [filterType]: valueProcessor(value),
           ...options,
         },
       };
@@ -16,20 +16,20 @@ function where(filterType, options = {}, valueFormatter = (value) => value) {
     return (queryObj, key, value) => {
       queryObj.where = {
         ...queryObj.where,
-        [key]: valueFormatter(value),
+        [key]: valueProcessor(value),
       };
     };
   }
 }
 
-function groupWhere(groupKey, key, valueFormatter = (value) => value) {
+function groupWhere(groupKey, key, valueProcessor = (value) => value) {
   // inputQueryParam is not used - we want the value to relate to the groupKey, not the req.query input param.
   return (queryObj, inputQueryParam, value) => {
     queryObj.where = {
       ...queryObj.where,
       [groupKey]: {
         ...queryObj.where?.[groupKey],
-        [key]: valueFormatter(value),
+        [key]: valueProcessor(value),
       },
     };
   };
