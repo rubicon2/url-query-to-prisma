0fbba0e25587b9b46aeecb60b703d5cb0c4df62c
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2025-06-13 15:51:00 +0100

    Implement foreignWhere formatter function

--- formatters.js
+++ formatters.js
@@ -1,3 +1,5 @@
+const pathToNestedObj = require('path-to-nested-obj');
+
 // Helper functions for customizing with customFormatter parameter.
 // Options on the text is basically just to allow mode: 'insensitive',
 // which is required on postgreSQL or mongoDB to case-insensitive filtering.
@@ -39,7 +41,33 @@ function groupWhere(groupKey, key, valueProcessor = (value) => value) {
   };
 }
 
+function foreignWhere(
+  filterType,
+  options = {},
+  valueProcessor = (value) => value,
+) {
+  if (filterType) {
+    return (queryObj, key, value) => {
+      queryObj.where = {
+        ...queryObj.where,
+        ...pathToNestedObj(key, '.', {
+          [filterType]: valueProcessor(value),
+          ...options,
+        }),
+      };
+    };
+  } else {
+    return (queryObj, key, value) => {
+      queryObj.where = {
+        ...queryObj.where,
+        ...pathToNestedObj(key, '.', valueProcessor(value)),
+      };
+    };
+  }
+}
+
 module.exports = {
   where,
   groupWhere,
+  foreignWhere,
 };
